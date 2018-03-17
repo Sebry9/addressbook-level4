39f51701e2e5f82ebccb2d6fda34f914bce40b03
Sebry9 <e0031720@u.nus.edu>
2018-03-17 11:13:17 +0800

    overload

--- src/main/java/seedu/address/storage/XmlAddressBookStorage.java
+++ src/main/java/seedu/address/storage/XmlAddressBookStorage.java
@@ -61,6 +61,15 @@ public class XmlAddressBookStorage implements AddressBookStorage {
         }
     }
 
+    public void saveAddressBook(ReadOnlyAddressBook addressBook, String filePath) throws IOException {
+        requireNonNull(addressBook);
+        requireNonNull(filePath);
+
+        File file = new File(filePath);
+        FileUtil.createIfMissing(file);
+        XmlFileStorage.saveDataToFile(file, new XmlSerializableAddressBook(addressBook));
+    }
+
     @Override
     public void saveAddressBook(ReadOnlyAddressBook addressBook) throws IOException {
         saveAddressBook(addressBook, filePath);
@@ -75,13 +84,6 @@ public class XmlAddressBookStorage implements AddressBookStorage {
      * Similar to {@link #saveAddressBook(ReadOnlyAddressBook)}
      * @param filePath location of the data. Cannot be null
      */
-    public void saveAddressBook(ReadOnlyAddressBook addressBook, String filePath) throws IOException {
-        requireNonNull(addressBook);
-        requireNonNull(filePath);
 
-        File file = new File(filePath);
-        FileUtil.createIfMissing(file);
-        XmlFileStorage.saveDataToFile(file, new XmlSerializableAddressBook(addressBook));
-    }
 
 }
